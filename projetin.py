974546d2fed592d827c059e7b8d524ed7c0273f2
IsaacDiniz77 <isaacdinix84@gmail.com>
2026-05-01 20:11:51 -0300

    codigo do meu amor!

---
+++ projetin.py
@@ -0,0 +1,30 @@
+#fazenda sertao 
+#Douglas Alencar Pereira Vieira P1 
+
+cliente = []
+adm = []
+
+
+while True: 
+    print("\n\nFazenda Sertao\n\n")
+
+    print("-"*50)
+
+    print('1-Criar uma conta ')
+    print('2- Fazer login ')
+    print("0- fechar programa ")
+
+    print("-"*50)
+    opcao1 = ("Digite a Opcao ")
+    if opcao1 == 0 : 
+        break
+    if opcao1 == 1 :
+        print("-"*50)
+
+        nome = input ( " Digite o seu nome completo : ")
+        email = input ( " Digite seu email : ")
+        usuario = input ( "Digite seu novo usuario : ")
+        senha = ("Digite sua Nova senha : ")
+        
+ 
+        print("-"*50)
